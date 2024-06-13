e66e347f94e53eb2a998c3f39203d460caf5cbfe
James Cuzella <james.cuzella@lyraphase.com>
2024-06-13 02:27:53 -0600

    hypernom_leaderboard: implement get_scores() + tests MVP

--- blockchain-soroban/contracts/hypernom_leaderboard/src/test.rs
+++ blockchain-soroban/contracts/hypernom_leaderboard/src/test.rs
@@ -79,10 +79,39 @@ fn test_add_score() {
     // let player2_scores = vec![(1, 5000),  (2, 200), (3, 300)];
     let expected = ();
     for (level, score) in player1_scores {
-        let mut result = client.add_score(&address1, &level, &score);
+        let result = client.add_score(&address1, &level, &score);
         assert_eq!(
             result,
             expected
         )
+    }
+}
+
+
+#[test]
+fn test_get_scores() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let contract_id = env.register_contract(None, HypernomLeaderboardContract);
+    let client = HypernomLeaderboardContractClient::new(&env, &contract_id);
+
+    let address = Address::generate(&env);
+    let player_name = String::from_str(&env, "Barney Rubble");
+    let _ = client.add_player(&player_name, &address); // Result<(), Error>
+    let player_scores = vec![&env, (0u32, 5200i64), (1u32, 11700i64), (2u32, 11500i64), (3u32, 28200i64), (4u32, 82900i64), (5u32, 204600i64)];
+
+    for (level, score) in player_scores.iter() {
+        let mut result = client.add_score(&address, &level, &score);
+    }
+
+    let result = client.get_scores(&address);
+    std::println!("Contract get_scores() returned: {:?}", result.as_ref());
+    std::assert_eq!(result.as_ref().unwrap().player_id, address);
+    std::assert_eq!(result.as_ref().unwrap().scores.get(0).unwrap(), 5200);
+    for (i, score) in (0u32..).zip(result.as_ref().unwrap().scores.iter()) {
+        std::println!("Level {}: {}", i, score);
+        std::println!("Level {}: {:?}", i, player_scores.get(i));
+        assert!(player_scores.get(i).is_some());
+        std::assert_eq!(score, player_scores.get(i).unwrap().1);
     }
 }
