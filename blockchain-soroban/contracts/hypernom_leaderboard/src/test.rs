1f76bb4486863478af6031581103500b8b0a22cc
James Cuzella <james.cuzella@lyraphase.com>
2024-06-13 00:22:02 -0600

    hypernom_leaderboard: implement get_list() test

--- blockchain-soroban/contracts/hypernom_leaderboard/src/test.rs
+++ blockchain-soroban/contracts/hypernom_leaderboard/src/test.rs
@@ -4,29 +4,10 @@ extern crate std; // for println! macro in tests ONLY
 use super::*;
 // use soroban_sdk::{symbol_short, vec, Env};
 use soroban_sdk::{testutils::Address as _, Address, Env};
-
-// #[test]
-// fn test() {
-//     let env = Env::default();
-//     let contract_id = env.register_contract(None, HelloContract);
-//     let client = HelloContractClient::new(&env, &contract_id);
-
-//     let words = client.hello(&symbol_short!("Dev"));
-//     std::println!("Contract hello() returned: {:?}", words);
-//     let expected = vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),];
-//     std::println!("Expected: {:?}", expected);
-//     let one_item = symbol_short!("Hello");
-//     std::println!("One item: {:?}", one_item);
-//     let one_item_vec = vec![&env, symbol_short!("Hello")];
-//     std::println!("One item Vec: {:?}", one_item_vec);
-//     assert_eq!(
-//         words,
-//         vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
-//     );
-// }
+//use soroban_sdk::testutils::arbitrary::{arbitrary, Arbitrary};
 
 #[test]
-fn test() {
+fn test_add_player() {
     let env = Env::default();
     env.mock_all_auths();
     let contract_id = env.register_contract(None, HypernomLeaderboardContract);
@@ -34,18 +15,40 @@ fn test() {
 
     let address = Address::generate(&env);
     let player_name = String::from_str(&env, "Barney Rubble");
-    // let result = client.add_player(&env, player_name, address); // Result<String, String>
-    let result = client.add_player(&player_name, &address); // Result<String, String>
-    std::println!("Contract hello() returned: {:?}", result);
-    // let expected = vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),];
-    // std::println!("Expected: {:?}", expected);
-    // let one_item = symbol_short!("Hello");
-    // std::println!("One item: {:?}", one_item);
-    // let one_item_vec = vec![&env, symbol_short!("Hello")];
-    // std::println!("One item Vec: {:?}", one_item_vec);
+    let result = client.add_player(&player_name, &address); // Result<(), Error>
+    std::println!("Contract add_player() returned: {:?}", result);
+
     std::assert_eq!(result, ());
-    // assert_eq!(
-    //     words,
-    //     vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
-    // );
+}
+
+#[test]
+fn test_get_list() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, HypernomLeaderboardContract);
+    let client = HypernomLeaderboardContractClient::new(&env, &contract_id);
+
+    let address1 = Address::generate(&env);
+    std::println!("Address: {:?}", address1);
+    let player1_name = String::from_str(&env, "Barney Rubble");
+    let player1_result = client.add_player(&player1_name, &address1); // Result<(), Error>
+    std::println!("Contract add_player() returned: {:?}", player1_result);
+
+    // Somehow arbitrary address generation is not working... so we'll just hardcode another randomly generated address
+    let address2 = Address::from_string(&String::from_str(&env, "GCXSYUGCPEW44PLHCPVZXVTSG4O6FKUSEYXJO5POCLSMD7MUZ4CNPMKV"));
+
+    let player2_name = String::from_str(&env, "Fred Flintstone");
+    let player2_result = client.add_player(&player2_name, &address2); // Result<(), Error>
+    std::println!("Contract add_player() returned: {:?}", player2_result);
+
+    let result = client.get_list();
+    std::println!("Contract get_list() returned: {:?}", result);
+
+    let mut expected = Map::new(&env);
+    expected.set(address1.clone(), player1_name.clone());
+    expected.set(address2.clone(), player2_name.clone());
+    assert_eq!(
+        result,
+        expected
+        // env.storage().persistent().get(&DataKey::Players).unwrap_or(Map::new(&env))
+    );
 }
